fe7e2640d72f29233b75fb564a1022fe5a896413
Cristian <garcia.cristian@outlook.com>
2022-05-24 02:11:47 -0500

    done

---
+++ main.py
@@ -0,0 +1,15 @@
+def get_n_longest_unique_words(words, n):
+    # Write your code here.
+    unique_words = []
+    for word in words:
+        if words.count(word) == 1:
+            unique_words.append(word)
+
+    longest_unique_words = sorted(unique_words, key=len, reverse=True)
+
+    return longest_unique_words[:n]
+
+
+words = ["Longer", "Whatever", "Longer", "Ball", "Rock", "Rocky", "Rocky"]
+
+print(get_n_longest_unique_words(words, 3))
